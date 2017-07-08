19e65c231f018e66f64aed0b0b133d568532f084
Jack Littleton <jack.littleton@gmail.com>
2017-07-08 15:16:56 -0700

    Setting up for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dstruct',
+      version='0.0.1',
+      description='DStruct lets you properly encapsulate your simple value objects so your interfaces are extensible',
+      url='http://github.com/dorkitude/dstruct',
+      author='Kyle Wild',
+      author_email='',
+      license='WTFPL',
+      packages=['dstruct'],
+      zip_safe=False)
